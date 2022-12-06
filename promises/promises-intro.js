b8be1288cd3cee8d3c286c6ccc88bfae2a35544e
Ankush Chauhan <ac.chauhan1102@gmail.com>
2022-12-07 00:13:42 +0530

    Day 6 : promise methods, beauty of .then

--- promises/promises-intro.js
+++ promises/promises-intro.js
@@ -64,3 +64,18 @@
 // };
 
 // callMe();
+
+//📃 Promise is object representing eventual completion of an
+// asynchrnous operation
+
+// Promise : {data: undefined}
+// empty object and after sometime data object will be filled automatically
+
+// BEAUTY OF .then
+// .then give us the guarantee, control of program to us, it give us trust
+// that code will fulfilled / rejected and will execute only once.
+// but with callback we don't have this trust and control over the program
+
+//Promise Chaining avoid callback hell,
+// it make code  evenly lean vertically
+// we can attach function to Promise Object by Promise Chaining, as incallback we were passing one f() to another f()
--- promises/promises-method.js
+++ promises/promises-method.js
@@ -42,6 +42,7 @@
 
 // Promise.all () return an array of all individual returned promises in same sequence it's passed,
 // if any of promises rejected, throws the error, which can be handled with .catch()
+// 🚨 all  promise are executing parallely, not like if one promise executes, rest all waiting for their turn
 
 /*
 
@@ -78,24 +79,56 @@
 
 */
 
-const promise1 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    reject("i am rejected 1");
-  }, 5000);
-});
-const promise3 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("foo2");
-  }, 1000);
-});
-const promise2 = Promise.resolve(42);
+// const promise1 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     reject("i am rejected 1");
+//   }, 5000);
+// });
+// const promise3 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("foo2");
+//   }, 1000);
+// });
+// const promise2 = Promise.resolve(42);
 
-Promise.any([promise1, promise2, promise3])
-  .then((values) => {
-    console.log(values);
-  })
-  .catch((err) => console.log("error occured ", err));
+// Promise.any([promise1, promise2, promise3])
+//   .then((values) => {
+//     console.log(values);
+//   })
+//   .catch((err) => console.log("error occured ", err));
 
 // 42
 
-//returns the fulfilled promise 
+//returns the fulfilled promise out of all array of Promises provided
+// it will choose the fast executing promise in case many fulfilled promises
+// Ignore rejecte promises
+
+/*
+----------------Promise.allSettled()-------------
+*/
+
+// const promise1 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     reject("i am rejected 1");
+//   }, 5000);
+// });
+// const promise3 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("foo2");
+//   }, 1000);
+// });
+// const promise2 = Promise.resolve(42);
+
+// Promise.allSettled([promise1, promise2, promise3])
+//   .then((values) => {
+//     console.log(values);
+//   })
+//   .catch((err) => console.log("error occured ", err));
+
+// return with an array of objects that describe the outcome of each promise
+// [
+//   { status: 'rejected', reason: 'i am rejected 1' },
+//   { status: 'fulfilled', value: 42 },
+//   { status: 'fulfilled', value: 'foo2' }
+// ]
+// total time of output is promise with longest execution time
